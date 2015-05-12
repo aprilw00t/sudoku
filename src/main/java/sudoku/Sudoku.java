914ca21c819573a33ea74523672a5bb065ecbbad
Edward.Wilson <Edward.Wilson@autotrader.co.uk>
2015-05-12 15:39:02 +0100

    Added first solution

--- src/main/java/sudoku/Sudoku.java
+++ src/main/java/sudoku/Sudoku.java
@@ -1,5 +1,55 @@
 package sudoku;
 
 public class Sudoku {
+	private int baseSize = 2;
+	private int[][] grid;
+
+	public Sudoku(int baseSize) {
+		this.baseSize = baseSize;
+		grid = new int[baseSize * baseSize][baseSize * baseSize];
+	}
+
+	public String toString() {
+		String s = "";
+		for (int[] row : grid) {
+			for (int cell : row) {
+				if (cell == 0) {
+					s += "_ ";
+				} else {
+					s += cell + " ";
+				}
+			}
+			s += "\n";
+		}
+		return s;
+	}
+
+
+	public boolean setValue(int x, int y, int value) {
+		for (int cell : grid[y]) {
+				if (cell == value) {
+					return false;
+				}
+		}
+
+		for (int[] row : grid) {
+			if (row[x] == value) {
+				return false;
+			}
+		}
+
+		int xQuadrantIndex = x / baseSize;
+		int yQuadrantIndex = y / baseSize;
+		for (int checkY = yQuadrantIndex * baseSize; checkY < (yQuadrantIndex + 1) * baseSize; checkY++) {
+			for (int checkX = xQuadrantIndex * baseSize; checkX < (xQuadrantIndex + 1) * baseSize; checkX++) {
+				if (grid[checkY][checkX] == value) {
+					return false;
+				}
+			}
+		}
+
+		grid[y][x] = value;
+		return true;
+	}
 
 }
--- src/test/java/sudoku/SudokuTest.java
+++ src/test/java/sudoku/SudokuTest.java
@@ -1,6 +1,13 @@
 package sudoku;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class SudokuTest {
 
@@ -8,7 +15,47 @@ public class SudokuTest {
 
 	@Before
 	public void setUp() {
-		sudoku = new Sudoku();
+		sudoku = new Sudoku(3);
+	}
+
+	@After
+	public void after() {
+		System.out.println(sudoku.toString());
+	}
+
+	@Test
+	public void printEmptySudokuGrid() {
+		sudoku = new Sudoku(2);
+		assertThat(sudoku.toString(), equalTo("_ _ _ _ \n_ _ _ _ \n_ _ _ _ \n_ _ _ _ \n"));
+	}
+
+	@Test
+	public void printValueOnFirstCell() {
+		sudoku = new Sudoku(2);
+		sudoku.setValue(0, 1, 1);
+		assertThat(sudoku.toString(), equalTo("_ _ _ _ \n1 _ _ _ \n_ _ _ _ \n_ _ _ _ \n"));
+	}
+
+	@Test
+	public void rowsShouldDisallowDuplicatesOnSameRow() {
+		assertTrue(sudoku.setValue(0, 0, 1));
+		assertFalse(sudoku.setValue(1, 0, 1));
+	}
+
+	@Test
+	public void rowsShouldDisallowDuplicatesOnSameColumn() {
+		assertTrue(sudoku.setValue(0, 0, 1));
+		assertFalse(sudoku.setValue(0, 1, 1));
+	}
+
+	@Test
+	public void rowsShouldDisallowDuplicatesInSameQuadrant() {
+		assertTrue(sudoku.setValue(1, 0, 1));
+		assertFalse(sudoku.setValue(0, 1, 1));
+
+		assertTrue(sudoku.setValue(6, 6, 2));
+		assertFalse(sudoku.setValue(8, 8, 2));
+
 	}
 
 }
